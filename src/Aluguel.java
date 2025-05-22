0af57c565a8ae4a8129d6deb1713f3116c826654
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-05-22 14:21:54 -0300

    criando listagem e registro de aluguel

--- src/Aluguel.java
+++ src/Aluguel.java
@@ -24,13 +24,13 @@ public class Aluguel {
         return data_formatada;
     }
 
-    public Aluguel(int quantidade_dias, LocalDate data_aluguel, LocalDate final_aluguel,Clientes cliente,Veiculos veiculo){
+    public Aluguel(int quantidade_dias, Clientes cliente, Veiculos veiculo){
         this.quantidade_dias = quantidade_dias;
         this.cliente = cliente;
         this.veiculo = veiculo;
         String data_atual = FormataData(LocalDate.now());
         this.data_aluguel = data_atual;
-        this.final_aluguel =  FormataData(CalculaDataFinal(quantidade_dias, data_aluguel));
+        this.final_aluguel =  FormataData(CalculaDataFinal(quantidade_dias, LocalDate.now()));
         this.preco_aluguel = quantidade_dias * veiculo.valor;
     }
     public void exibirAlugueis(){
